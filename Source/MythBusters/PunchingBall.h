0bef23dfadee1cad44edecdd4e9a9573de3fd1b3
Louis <louis.ginesy@telecom-sudparis.eu>
2020-10-12 00:22:15 +0200

    Ajout des classe c++ de punching ball

---
+++ Source/MythBusters/PunchingBall.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "god.h"
+#include "PunchingBall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHBUSTERS_API APunchingBall : public Agod
+{
+	GENERATED_BODY()
+	
+};
