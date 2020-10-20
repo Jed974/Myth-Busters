8143332856251ca50f85235939624b4d14ae2cdd
Louis <louis.ginesy@telecom-sudparis.eu>
2020-10-20 19:50:06 +0200

    Creation of StartAttackInteruptable in c++

--- Source/MythBusters/GodAnimInstance.cpp
+++ Source/MythBusters/GodAnimInstance.cpp
@@ -22,3 +22,41 @@ void UGodAnimInstance::NativeUpdateAnimation(float DeltaSeconds) {
 		}
 	}	
 }
+
+void UGodAnimInstance::InterruptAttack(bool hurting, bool ejecting) {
+	AttCanStop = true;
+	AttNormal = false;
+	AttSpe = false;
+	AttPush = false;
+
+	if (hurting)
+		Hurted = true;
+	else if (ejecting)
+		Ejected = true;
+
+	// TODO :: Ne marche pas toujours... Des fois le groupe reste dans la scène et jsp pourquoi
+	if (InterruptableAttGroup != nullptr) {
+		bool destroySuccess = InterruptableAttGroup->Destroy();
+		if (destroySuccess)
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Destroy successful");
+		else
+			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Destroy failed");
+	}
+	else
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "No group to destroy");
+}
+
+void UGodAnimInstance::StartAttackInterruptable(TSubclassOf<AHitBoxGroup> ClassToSpwan, FName SocketToAttachTo) {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Attack Started !");
+
+	FTransform _spawnTransform = God->GetRootComponent()->GetSocketTransform(SocketToAttachTo);
+	_spawnTransform.SetScale3D(FVector(1, 1, 1));
+
+	FActorSpawnParameters _spawnParams;
+	_spawnParams.Instigator = God;
+
+	InterruptableAttGroup = GetWorld()->SpawnActor<AHitBoxGroup>(ClassToSpwan, _spawnTransform.GetLocation(), _spawnTransform.GetRotation().Rotator(), _spawnParams);
+
+	FAttachmentTransformRules _attTransformRules(EAttachmentRule::SnapToTarget, EAttachmentRule::SnapToTarget, EAttachmentRule::KeepWorld, false);
+	InterruptableAttGroup->AttachToComponent(God->GetSkeletalMesh(), _attTransformRules, SocketToAttachTo);
+}
