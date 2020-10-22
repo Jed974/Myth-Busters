e50db457890a7abe5126dd6455108fecbaa1dd32
Louis <louis.ginesy@telecom-sudparis.eu>
2020-10-22 18:03:45 +0200

    Implementing the Arena Actor

--- Source/MythBusters/Arena.h
+++ Source/MythBusters/Arena.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+//#include "ArenaBorderElement.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Arena.generated.h"
@@ -14,6 +15,19 @@ class MYTHBUSTERS_API AArena : public AActor
 public:	
 	// Sets default values for this actor's properties
 	AArena();
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, CATEGORY = "Components", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* Root;
+
+	//UPROPERTY(BlueprintReadOnly, VisibleAnywhere, CATEGORY = "Components", meta = (AllowPrivateAccess = "true"))
+	//	UArenaBorderElement* Element0;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, CATEGORY = "Components", meta = (AllowPrivateAccess = "true"))
+		TArray<UStaticMeshComponent*> Grounds;
+
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, CATEGORY = "Components", meta = (AllowPrivateAccess = "true"))
+		USceneComponent* FoliageRoot;
+
 
 protected:
 	// Called when the game starts or when spawned
