42bcfe82b57c5436f2aaf8c725c183a7507025d1
Jed974 <48924190+Jed974@users.noreply.github.com>
2021-01-18 12:45:57 +0100

    Fix dash

--- Source/MythBusters/god.cpp
+++ Source/MythBusters/god.cpp
@@ -103,6 +103,14 @@ void AGod::AttackNormal()
 	switch (State)
 	{
 	case EGodState::Flying:
+		//GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Purple, "Flying");
+		if (GodAttack->StartNormalAttack(attackState)) {
+			EAttackNormal();
+			ChangeGodState(EGodState::Attacking);
+		}
+		break;
+	case EGodState::FlyingTurnaround:
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Purple, "Backward attack");
 		if (GodAttack->StartNormalAttack(attackState)) {
 			EAttackNormal();
 			ChangeGodState(EGodState::Attacking);
