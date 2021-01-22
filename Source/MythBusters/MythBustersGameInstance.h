6e347b6ca3289a0bb072d8a82b2c49ed16711a43
paul.ciupek <paul.ciupek@telecom-sudparis.eu>
2021-01-22 15:28:13 +0100

    Tentative de communication de la selection des dieux

--- Source/MythBusters/MythBustersGameInstance.h
+++ Source/MythBusters/MythBustersGameInstance.h
@@ -19,118 +19,118 @@
 
 struct AbstractCharacter
 {
-    AGod* ref;
-    FTransform transform;
-    FVector2D velocity;
-    float damage;
+	AGod* ref;
+	FTransform transform;
+	FVector2D velocity;
+	float damage;
 };
 
 struct AbstractGameState
 {
-    TArray<AbstractCharacter> characters;
-    int _framenumber = 0;
-    void Init(int num_players, UGameInstance* Instance)
-    {
-        for (int i = 0; i < num_players; i++)
-        {
-            AGod* god = (AGod*)Instance->GetLocalPlayers()[i]->PlayerController->GetPawn();
-            AbstractCharacter AbstractGod = AbstractCharacter();
-            AbstractGod.ref = god;
-            AbstractGod.transform = god->GetActorTransform();
-            AbstractGod.velocity = god->GetGodMovementComponent()->Velocity;
-            AbstractGod.damage = god->GodDamage;
-            characters.Add(AbstractGod);
-        }
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "GameState Initiated");
+	TArray<AbstractCharacter> characters;
+	int _framenumber = 0;
+	void Init(int num_players, UGameInstance* Instance)
+	{
+		for (int i = 0; i < num_players; i++)
+		{
+			AGod* god = (AGod*)Instance->GetLocalPlayers()[i]->PlayerController->GetPawn();
+			AbstractCharacter AbstractGod = AbstractCharacter();
+			AbstractGod.ref = god;
+			AbstractGod.transform = god->GetActorTransform();
+			AbstractGod.velocity = god->GetGodMovementComponent()->Velocity;
+			AbstractGod.damage = god->GodDamage;
+			characters.Add(AbstractGod);
+		}
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "GameState Initiated");
 
-    }
-    void Apply()
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Rollback !");
-        for (int i = 0; i < characters.Num(); i++)
-        {
-            if (characters[i].ref != nullptr)
-            {
+	}
+	void Apply()
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Rollback !");
+		for (int i = 0; i < characters.Num(); i++)
+		{
+			if (characters[i].ref != nullptr)
+			{
 
-                characters[i].ref->SetActorTransform(characters[i].transform);
-                characters[i].ref->GetGodMovementComponent()->Velocity = characters[i].velocity;
-                characters[i].ref->GodDamage = characters[i].damage;
-            }
+				characters[i].ref->SetActorTransform(characters[i].transform);
+				characters[i].ref->GetGodMovementComponent()->Velocity = characters[i].velocity;
+				characters[i].ref->GodDamage = characters[i].damage;
+			}
 
-        }
-    };
+		}
+	};
 
-    void Observe()
-    {
-        for (int i = 0; i < characters.Num(); i++)
-        {
-            if (characters[i].ref != nullptr)
-            {
-                //GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Saving Character " + FString::FromInt(i));
-                characters[i].transform = characters[i].ref->GetActorTransform();
-                characters[i].velocity = characters[i].ref->GetGodMovementComponent()->Velocity;
-                characters[i].damage = characters[i].ref->GodDamage;
-            }
+	void Observe()
+	{
+		for (int i = 0; i < characters.Num(); i++)
+		{
+			if (characters[i].ref != nullptr)
+			{
+				//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Saving Character " + FString::FromInt(i));
+				characters[i].transform = characters[i].ref->GetActorTransform();
+				characters[i].velocity = characters[i].ref->GetGodMovementComponent()->Velocity;
+				characters[i].damage = characters[i].ref->GodDamage;
+			}
 
-        }
-    };
+		}
+	};
 };
 
 
 struct MYTHBUSTERS_API SAbstractGameState
 {
-    TArray<SAbstractGod> Gods;
-    int _framenumber = 0;
-    void Init(int num_players, UGameInstance* Instance)
-    {
-        for (int i = 0; i < num_players; i++)
-        {
-            AGod* god = (AGod*)Instance->GetLocalPlayers()[i]->PlayerController->GetPawn();
-            SAbstractGod AbstractGod = SAbstractGod();
-            AbstractGod.Init(god);
-            Gods.Add(AbstractGod);
-        }
-        GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "GameState Initiated");
+	TArray<SAbstractGod> Gods;
+	int _framenumber = 0;
+	void Init(int num_players, UGameInstance* Instance)
+	{
+		for (int i = 0; i < num_players; i++)
+		{
+			AGod* god = (AGod*)Instance->GetLocalPlayers()[i]->PlayerController->GetPawn();
+			SAbstractGod AbstractGod = SAbstractGod();
+			AbstractGod.Init(god);
+			Gods.Add(AbstractGod);
+		}
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "GameState Initiated");
 
-    }
-    void Apply()
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Rollback !");
-        for (int i = 0; i < Gods.Num(); i++)
-        {
-            if (Gods[i].Ref != nullptr)
-            {
-                Gods[i].Apply();
-                //Gods[i].Ref->SetActorTransform(Gods[i].Transform);
-                //Gods[i].ref->GetGodMovementComponent()->Velocity = Gods[i].velocity;
-                //Gods[i].ref->GodDamage = Gods[i].damage;
-            }
+	}
+	void Apply()
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Rollback !");
+		for (int i = 0; i < Gods.Num(); i++)
+		{
+			if (Gods[i].Ref != nullptr)
+			{
+				Gods[i].Apply();
+				//Gods[i].Ref->SetActorTransform(Gods[i].Transform);
+				//Gods[i].ref->GetGodMovementComponent()->Velocity = Gods[i].velocity;
+				//Gods[i].ref->GodDamage = Gods[i].damage;
+			}
 
-        }
+		}
 
 
-    }
-    void Observe()
-    {
-        for (int i = 0; i < Gods.Num(); i++)
-        {
-            if (Gods[i].Ref != nullptr)
-            {
-                Gods[i].Observe();
-                //GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Saving Character " + FString::FromInt(i));
-                //Gods[i].Transform = Gods[i].Ref->GetActorTransform();
-                //characters[i].velocity = characters[i].ref->GetGodMovementComponent()->Velocity;
-                //characters[i].damage = characters[i].ref->GodDamage;
-            }
+	}
+	void Observe()
+	{
+		for (int i = 0; i < Gods.Num(); i++)
+		{
+			if (Gods[i].Ref != nullptr)
+			{
+				Gods[i].Observe();
+				//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, "Saving Character " + FString::FromInt(i));
+				//Gods[i].Transform = Gods[i].Ref->GetActorTransform();
+				//characters[i].velocity = characters[i].ref->GetGodMovementComponent()->Velocity;
+				//characters[i].damage = characters[i].ref->GodDamage;
+			}
 
-        }
+		}
 
-    }
+	}
 };
 
 
 /**
- * 
+ *
  */
 UCLASS()
 class MYTHBUSTERS_API UMythBustersGameInstance : public UGameInstance
@@ -139,7 +139,7 @@ class MYTHBUSTERS_API UMythBustersGameInstance : public UGameInstance
 
 protected:
 	UMythBustersGameInstance();
-    ~UMythBustersGameInstance();
+	~UMythBustersGameInstance();
 
 public:
 
@@ -152,41 +152,41 @@ public:
 	static void Static_MythBusters_DisconnectPlayer(int player);
 	static void Static_MythBusters_Exit();*/
 
-    /*static bool __cdecl mb_begin_game_callback(const char*);
-    static bool __cdecl mb_on_event_callback(GGPOEvent* info);
-    static bool __cdecl mb_advance_frame_callback(int);
-    static bool __cdecl mb_load_game_state_callback(unsigned char* buffer, int len);
-    static bool __cdecl mb_save_game_state_callback(unsigned char** buffer, int* len, int* checksum, int);
-    static void __cdecl mb_free_buffer_callback(void* buffer);*/
+	/*static bool __cdecl mb_begin_game_callback(const char*);
+	static bool __cdecl mb_on_event_callback(GGPOEvent* info);
+	static bool __cdecl mb_advance_frame_callback(int);
+	static bool __cdecl mb_load_game_state_callback(unsigned char* buffer, int len);
+	static bool __cdecl mb_save_game_state_callback(unsigned char** buffer, int* len, int* checksum, int);
+	static void __cdecl mb_free_buffer_callback(void* buffer);*/
 
-    void MythBusters_Init(unsigned short localport, int num_players, TArray<GGPOPlayer> players, int num_spectators);
-    //void MythBusters_InitSpectator(unsigned short localport, int num_players, char* host_ip, unsigned short host_port);
-    //void MythBusters_DrawCurrentFrame();
-    void MythBusters_AdvanceFrame(SSendableInputs inputs[], int disconnect_flags);
-    UFUNCTION(BlueprintCallable)
-    void MythBusters_RunFrame();
-    //void MythBusters_Idle(int time);
-    void MythBusters_DisconnectPlayer(int player);
-    void MythBusters_Exit();
+	void MythBusters_Init(unsigned short localport, int num_players, TArray<GGPOPlayer> players, int num_spectators);
+	//void MythBusters_InitSpectator(unsigned short localport, int num_players, char* host_ip, unsigned short host_port);
+	//void MythBusters_DrawCurrentFrame();
+	void MythBusters_AdvanceFrame(SSendableInputs inputs[], int disconnect_flags);
+	UFUNCTION(BlueprintCallable)
+		void MythBusters_RunFrame();
+	//void MythBusters_Idle(int time);
+	void MythBusters_DisconnectPlayer(int player);
+	void MythBusters_Exit();
 
-    SAbstractGameState gs;
-    NonGameState ngs;
-    GGPOSession* ggpo;
-    static UMythBustersGameInstance* Instance;
-    SSendableInputs Inputs[MAX_PLAYERS];
-    int PacketSize;
+	SAbstractGameState gs;
+	NonGameState ngs;
+	GGPOSession* ggpo;
+	static UMythBustersGameInstance* Instance;
+	SSendableInputs Inputs[MAX_PLAYERS];
+	int PacketSize;
 
 	unsigned char* _buffer;
 	int _len;
 	int _checksum;
 
-    UFUNCTION(BlueprintCallable)
-    void LoadState();
-    UFUNCTION(BlueprintCallable)
-    void SaveState();
+	UFUNCTION(BlueprintCallable)
+		void LoadState();
+	UFUNCTION(BlueprintCallable)
+		void SaveState();
 
 	UFUNCTION(BlueprintCallable)
-	void InitState();
+		void InitState();
 
 	TArray<GGPOPlayer> Players;
 
@@ -194,25 +194,25 @@ public:
 	void CreateLocalPlayer();*/
 
 	UFUNCTION(BlueprintCallable)
-	void CreateRemotePlayer(FString IPAdress);
+		void CreateRemotePlayer(FString IPAdress);
 
 	UFUNCTION(BlueprintCallable)
-	void StartGGPO();
+		void StartGGPO();
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    int GGPOPlayerIndex;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int GGPOPlayerIndex;
 
-    UFUNCTION(BlueprintCallable)
-    void MythBusters_Idle(int timeout);
+	UFUNCTION(BlueprintCallable)
+		void MythBusters_Idle(int timeout);
 
-    UFUNCTION(BlueprintCallable)
-    void MythBusters_NextFrame();
+	UFUNCTION(BlueprintCallable)
+		void MythBusters_NextFrame();
 
-    void MainThreadSleep(float time);
+	void MainThreadSleep(float time);
 
-    UPROPERTY(BlueprintReadWrite, EditAnywhere)
-    bool rollbacking = false;
-    bool InputsReadyForFrame = false;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		bool rollbacking = false;
+	bool InputsReadyForFrame = false;
 
 	/*bool __cdecl mb_advance_frame_callback(int);
 	bool __cdecl mb_load_game_state_callback(unsigned char* buffer, int len);
@@ -220,8 +220,8 @@ public:
 
 	//UMythBustersGameInstance* Instance;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-        TArray<int> SelectedGods = { -1, -1 }; // 0 = god local; 1 = god distant
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<int> SelectedGods = { 0, -1 }; // 0 = god local; 1 = god distant
 
-    const SInputs ThorSelectedCode;
+	char ThorSelectedCode = 0b01111111;
 };
